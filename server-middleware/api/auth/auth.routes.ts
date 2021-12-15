e6add909ea5397c524f710dd07c4060dc0a1ded7
Mihai Andrei <mihai.andrei1597@gmail.com>
2021-12-15 12:07:39 +0200

    added refresh token logic with cookies

--- server-middleware/api/auth/auth.routes.ts
+++ server-middleware/api/auth/auth.routes.ts
@@ -7,6 +7,7 @@ import {
   findUserByEmail,
 } from "../users/users.services";
 import { generateTokens } from "~/utils/jwt";
+import { sendRefreshToken } from "~~/utils/sendRefreshToken";
 
 const router = express.Router();
 
@@ -34,15 +35,57 @@ router.post("/register", async (req, res, next) => {
 
     const { accessToken, refreshToken } = generateTokens(user);
 
+    sendRefreshToken(res, refreshToken);
+
     res.json({
       accessToken,
-      refreshToken,
     });
   } catch (error) {
     if (res.statusCode === 200) {
       res.status(403);
     }
     console.log(error);
+    res.status(403).json({ message: error.message });
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(403);
+      throw new Error("Email or password is empty.");
+    }
+
+    const createUser: Prisma.UserCreateInput = { email, password };
+
+    const existingUser = await findUserByEmail(email);
+
+    if (!existingUser) {
+      res.status(403);
+      throw new Error("Invalid login credentials.");
+    }
+
+    const validPassword = await bcrypt.compare(password, existingUser.password);
+
+    if (!validPassword) {
+      const error = new Error("Invalid login credentials.");
+      res.status(403);
+      throw error;
+    }
+
+    const { accessToken, refreshToken } = generateTokens(existingUser);
+    sendRefreshToken(res, refreshToken);
+
+    res.json({
+      accessToken,
+    });
+  } catch (error) {
+    if (res.statusCode === 200) {
+      res.status(403);
+    }
+
     res.status(403).json({ message: error.message });
   }
 });
--- utils/sendRefreshToken.ts
+++ utils/sendRefreshToken.ts
@@ -1,8 +1,10 @@
 import { Response } from "express";
+import { setCookie } from "h3";
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie("refresh_token", token, {
+  setCookie(res, "refresh_token", token, {
     httpOnly: true,
+    sameSite: true,
     path: "server-api/auth/v1/refresh_token",
   });
 };
